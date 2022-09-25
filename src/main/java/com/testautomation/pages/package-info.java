fd06d1733a385c4b867e3942757435232a28cbd6
RokayaSafawat <rokaya@dxpr.com>
2022-09-25 16:15:31 +0200

    added pachage-info file for documenting page package

---
+++ src/main/java/com/testautomation/pages/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides the Page object model classes necessary to identify the WebElements of
+ * each web page and also contains Page methods which perform operations on
+ * those WebElements.
+ */
+
+package com.testautomation.pages;
