f1fcb134f65893ad85a7b76176540af6f99a2bcd
RokayaSafawat <rokaya@dxpr.com>
2022-09-25 16:23:19 +0200

    added pacjage-info file that documeting class conatined in utility package

--- src/main/java/com/testautomation/utility/package-info.java
+++ src/main/java/com/testautomation/utility/package-info.java
@@ -2,6 +2,10 @@
  * Provides the Browser class that contains a bunch of methods, so they can be
  * reused across all test automation files. Provides the PropertiesFileReader
  * that contains methods to get data from the browser-config properties file
+ * Provides the JsonDataReader that contains methods to get data from json files
+ * Provides the JavaScriptMethods that contains common javascript methods
+ * Provides the SynchronizationMethods that contains common selenium Synchronization methods
+ *  Provides the TestDataProvider that contains methods that parsing test data from json files
  */
 
 package com.testautomation.utility;
